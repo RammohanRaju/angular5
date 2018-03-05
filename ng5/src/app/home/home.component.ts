a426627ddb94d9b811293bed252d457014ea450a
Ramamohan RajuGadiraju <rammohan2419@gmail.com>
2018-03-05 14:39:22 +0530

    Add services

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -31,24 +32,28 @@ export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first goal';
-  goals = ["First goal", "Second goal", "Third goal"];
+  goals = [];
 
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
-  	this.itemCount = this.goals.length;
+    this._data.goal.subscribe(res => this.goals = res);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   addItem(){
   	this.goals.push(this.goalText);
   	this.goalText = '';
-  	this.itemCount = this.goals.length;
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i){
     this.goals.splice(i,1);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
 }
